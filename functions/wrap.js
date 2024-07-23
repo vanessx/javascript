1efd7ceeea3e108fcb0b30168c5a076138aa806d
urassaya <vanessafoxandrade@hotmail.com>
2024-07-23 20:23:15 +0200

    Create wrap.js

---
+++ functions/wrap.js
@@ -0,0 +1,16 @@
+function wrapValue(n) {
+  let local = n;
+  return () => local;
+}
+
+let wrap1 = wrapValue(5);
+let wrap2 = wrapValue(2);
+console.log(wrap1());
+console.log(wrap2());
+
+function multiplier(factor) {
+  return (number) => number * factor;
+}
+
+let twice = multiplier(2);
+console.log(twice(5));
